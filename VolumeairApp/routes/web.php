afa2707719000ff7ab6ad9e16963d3295bdbbec5
andreferdinan1 <andreferdinand276@gmail.com>
2025-08-23 18:09:37 +0700

    .

--- VolumeairApp/routes/web.php
+++ VolumeairApp/routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/volumeair', function () {
+Route::get('/', function () {
     return view('volumeair.dashboard'); // file Blade kamu
 })->name('volumeair.dashboard');
 
